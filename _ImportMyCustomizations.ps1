0b28f47d1003c7b937e83d34b057f2306321c04a
Amr Eldib <amr@amreldib.com>
2018-08-12 20:33:06 -0700

    Added customizations for ThisPC and Desktop List

--- _ImportMyCustomizations.ps1
+++ _ImportMyCustomizations.ps1
@@ -1,9 +1,11 @@
-$customizationsFile = "_MyCustomizations.txt"
+$scriptPath = split-path -parent $MyInvocation.MyCommand.Definition
+$customizationsFile = Join-Path $scriptPath "_MyCustomizations.txt" 
 
 if (Test-Path $customizationsFile) { 
     foreach($line in Get-Content $customizationsFile) {
         if ($line.EndsWith(".reg")) {
-            Invoke-Expression 'reg import "$line"'
+            $file = Join-Path $scriptPath .\$line
+            Invoke-Expression 'reg import "$file"'
             Write-Output "Imported '$line'"
         }
     }
